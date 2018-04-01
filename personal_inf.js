d81dfa7b6fda150a66f1b5e0a7683eae0cecbb45
NEWESTERS <ramiro.cruz@mail.ru>
2018-04-01 19:18:23 +0300

    overflow hidden

--- personal_inf.js
+++ personal_inf.js
@@ -1,24 +1,31 @@
 function portfolioOnload( link ) {
-			var element = document.getElementsByClassName(link)[0];
-		
-			
-			this.show_block = function () {
-				var h = window.innerHeight;
-				element.setAttribute('shown','yes');
-				element.style.height = h + 'px';
-				$('html,body').animate({scrollTop:h},700);
-			}
+	var element = document.getElementsByClassName(link)[0];
 
-			this.show_components = function( property , value ){
-				element.setAttribute( property , value );
-			}
+	
+	this.show_block = function () {
+		var h = window.innerHeight;
+		element.setAttribute('shown','yes');
+		element.style.height = h + 'px';
+		// $('html,body').animate({scrollTop:h},700);
+		$(document.body).animate({
+    		'scrollTop':   $('#portfolio').offset().top
+		}, 2000);
+	}
+
+	this.show_components = function( property , value ){
+		element.setAttribute( property , value );
+	}
 			
 };
 
 window.onload = function (){
 	blockk = new portfolioOnload("portfolio");
-	but = document.getElementById('clickme');
-	but.onclick = function(){
+	dog = document.getElementById('clickme');
+	dog.onclick = function(){
 		blockk.show_block();
 	};
-};		
+};	
+
+
+
+
