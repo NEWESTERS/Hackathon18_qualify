1a15a3933f92218afff4eed7da6f8fd65c7df9f2
NEWESTERS <ramiro.cruz@mail.ru>
2018-04-03 22:29:08 +0300

    new photos

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onload = function() {
 	var pager = new function() {
 		var number = 0;
 
+		// Функция, обновляющая данные об участнике
 		function update(number) {
 			$('#bio').text(data[number].bio);
 			$('#name').html('<h2>' + data[number].name + '</h2>');
@@ -11,11 +12,13 @@ window.onload = function() {
 			}
 		}
 
+		// Следующий участник
 		this.next = function() {
 			number = number == 4 ? 0 : number+=1;
 			update(number);
 		}
 
+		// Предыдущий участник
 		this.back = function() {
 			number = number == 0 ? 4 : number-=1;
 			update(number);
@@ -24,34 +27,56 @@ window.onload = function() {
 		update(number);
 	}
 
+	// Инициализация библиотеки для появления блоков при скроллинге
 	AOS.init();
 
+	// Клик на собаку скроллит страницу на блок с участниками
 	$(document).on('click', '#clickme', function() {
 		$(document.body).animate({
     		'scrollTop':   $('#team').offset().top
 		}, 800);
 	});
 
+	// Клик на стрелку влево
 	$(document).on('click', '#left-arrow', function() {
-		$('#info').animate({
-			opacity: 0
-		}, 250, function(){
+		/*
+		$('#info').addClass('scroll-left-begin');
+		setTimeout(function() {
 			pager.back();
+			$('#info').removeClass('scroll-left-begin');
+			$('#info').addClass('scroll-left-end');
+		}, 250);
+		*/
+		
+		$('#info').animate({
+			'margin-right': '+=100px',
+			'margin-left': '-=100px',
+			opacity: 0
+		}, 300, function(){
+			pager.back();
+			$('#info').css('margin-right', '0');
+			$('#info').css('margin-left', '0');
 		});
 		$('#info').animate({
 			opacity: 1
-		}, 250);
+		}, 300);
+		
 	});
 
+	// Клик на стрелку вправо
 	$(document).on('click', '#right-arrow', function() {
 		$('#info').animate({
+			'margin-left': '+=100px',
+			'margin-right': '-=100px',
 			opacity: 0
-		}, 250, function() {
+		}, 300, function() {
 			pager.next();
+			$('#info').css('margin-left', '0');
+			$('#info').css('margin-right', '0');
 		});
 		$('#info').animate({
 			opacity: 1
-		}, 250);
+		}, 300);
 	})
 	
 };	
